6f690cd88468fdd62ae53be8f4e4627144c7b1c4
joaoFDev03 <joaopedrofer1509@gmail.com>
2025-11-04 19:33:40 +0000

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,24 +1,3 @@
-// import express from 'express';
-// import bloqRoutes from './routes/bloq.routes.js';
-// import lockerRoutes from './routes/locker.routes.js';
-// import rentRoutes from './routes/rent.routes.js';
-// import { errorHandler } from './middleware/errorHandler.js';
-// import bodyParser from 'body-parser';
-// const app = express();
-
-// app.use(express.json());
-// app.use(bodyParser.json())
-
-
-// app.use('/bloqs', bloqRoutes);
-// app.use('/lockers', lockerRoutes);
-// app.use('/rents', rentRoutes);
-
-
-// app.use(errorHandler);
-
-// export default app;
-
 import express from "express";
 
 const app = express()
